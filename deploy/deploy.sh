e342e22fbafe4b33fcd62aa8acafee1b5a9b6152
David Donnelly <Daviddonnellydev@gmail.com>
2024-02-04 10:59:06 -0500

    fix error in variable reference in build scripts

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,7 +4,7 @@ kube_deploy() {
     local FILE_NAME="$1"
 
     echo "$(date): Posting ${FILE_NAME} to the K8 API server."
-    kubectl apply -f "../k8-manifests/$FILE_NAME"
+    kubectl apply -f "../k8-manifests/${FILE_NAME}"
 }
 
 
