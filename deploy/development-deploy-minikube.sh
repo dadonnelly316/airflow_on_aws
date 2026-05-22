ab425bba0231221b173c7f7183803ea49bee5137
David Donnelly <Daviddonnellydev@gmail.com>
2026-05-22 07:17:00 -0400

    feat: airflow 3.0 upgrade - remove deprecated webserver component and replace with api-server

--- deploy/development-deploy-minikube.sh
+++ deploy/development-deploy-minikube.sh
@@ -27,36 +27,36 @@ kube_deploy() {
 kube_deploy "init-role.yaml"
 kube_deploy "init-service-account.yaml"
 kube_deploy "init-svc-role-binding.yaml" 
-kube_deploy "airflow-webserver-service.yaml"
-kube_deploy "airflow-webserver-ingress.yaml" 
+kube_deploy "airflow-api-server-service.yaml"
+kube_deploy "airflow-api-server-ingress.yaml" 
 
 # creating secrets referenced in K8 manifest files
-kubectl delete secret database-connection-string airflow-webserver-username airflow-webserver-password airflow-webserver-email airflow-webserver-secret-key --ignore-not-found
+kubectl delete secret database-connection-string airflow-api-server-username airflow-api-server-password airflow-api-server-email airflow-api-server-secret-key --ignore-not-found
 kubectl create secret generic "database-connection-string" --from-literal="database-connection-string"="${POSTGRES_CONN_STRING}" 
-kubectl create secret generic "airflow-webserver-username" --from-literal="airflow-webserver-username"="non-prod-testing" 
-kubectl create secret generic "airflow-webserver-password" --from-literal="airflow-webserver-password"="youshallpass" 
-kubectl create secret generic "airflow-webserver-email" --from-literal="airflow-webserver-email"="shrek@getoutofmyswamp.com" 
-kubectl create secret generic "airflow-webserver-secret-key" --from-literal="airflow-webserver-secret-key"="12345" 
+kubectl create secret generic "airflow-api-server-username" --from-literal="airflow-api-server-username"="non-prod-testing" 
+kubectl create secret generic "airflow-api-server-password" --from-literal="airflow-api-server-password"="youshallpass" 
+kubectl create secret generic "airflow-api-server-email" --from-literal="airflow-api-server-email"="shrek@getoutofmyswamp.com" 
+kubectl create secret generic "airflow-api-server-secret-key" --from-literal="airflow-api-server-secret-key"="12345" 
 
 # we are taking K8 manifest files that are acting as "templates", copying them to a temp directory where it will be reference in kubectl apply, and using sed to configure manifest to work on minikube
 mkdir ../k8-manifests/_tmp
-cp ./{../k8-manifests/airflow-scheduler-deployment.yaml,../k8-manifests/airflow-webserver-deployment.yaml,../k8-manifests/airflow-config-map.yaml} ../k8-manifests/_tmp
+cp ./{../k8-manifests/airflow-scheduler-deployment.yaml,../k8-manifests/airflow-api-server-deployment.yaml,../k8-manifests/airflow-config-map.yaml} ../k8-manifests/_tmp
 
 # using , as delimiter since we have slashes in the ingress host
 sed -i '' 's,__INGRESS_HOST__,http://localhost:80,g' ../k8-manifests/_tmp/airflow-config-map.yaml
 
 # We must set image pull policy to never (see tip 1 https://minikube.sigs.k8s.io/docs/handbook/pushing/)
-sed -i '' 's,__IMAGE_PULL_POLICY__,Never,g' ../k8-manifests/_tmp/airflow-webserver-deployment.yaml
+sed -i '' 's,__IMAGE_PULL_POLICY__,Never,g' ../k8-manifests/_tmp/airflow-api-server-deployment.yaml
 sed -i '' 's/__IMAGE_PULL_POLICY__/Never/g' ../k8-manifests/_tmp/airflow-scheduler-deployment.yaml
 
 # find image in local docker repository
-sed -i '' 's,__AIRFLOW_IMAGE__,airflow:latest,g' ../k8-manifests/_tmp/airflow-webserver-deployment.yaml
+sed -i '' 's,__AIRFLOW_IMAGE__,airflow:latest,g' ../k8-manifests/_tmp/airflow-api-server-deployment.yaml
 sed -i '' 's,__AIRFLOW_IMAGE__,airflow:latest,g' ../k8-manifests/_tmp/airflow-scheduler-deployment.yaml
 
-kubectl delete deployments --ignore-not-found=true airflow-scheduler airflow-webserver
+kubectl delete deployments --ignore-not-found=true airflow-scheduler airflow-api-server
 kube_deploy "_tmp/airflow-config-map.yaml"
 kube_deploy "_tmp/airflow-scheduler-deployment.yaml"
-kube_deploy "_tmp/airflow-webserver-deployment.yaml"
+kube_deploy "_tmp/airflow-api-server-deployment.yaml"
 
 
 rm -rf ./_tmp
