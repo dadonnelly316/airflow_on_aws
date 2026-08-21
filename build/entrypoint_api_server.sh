a83fa269eb3cae9d10a109085740d7ddde3c495f
David Donnelly <Daviddonnellydev@gmail.com>
2026-08-21 17:17:23 -0400

    refactor: simple auth manager will be used locally. airflow users create was deprecated. in prod environments a sso form of auth should be used via fab.

--- build/entrypoint_api_server.sh
+++ build/entrypoint_api_server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# AIRFLOW DB CHECK COMMAND SETTINGS
 INPUT_MAX_RETRIES=120
 INPUT_RETRY_DELAY=1
 echo "$(date): Checking if the airflow database can be reached."
@@ -11,7 +10,6 @@ bash ./build/airflow_db_check.sh $INPUT_MAX_RETRIES $INPUT_RETRY_DELAY
 # todo - handle failures of db check better. Check state of other pod useing kubectl, and possible kill other pod
 
 
-# https://airflow.apache.org/docs/apache-airflow/stable/cli-and-env-variables-ref.html#check-migrations
 if [[ $RUN_DB_MIGRATION_BOOLEAN=='1' ]]; then
     sleep 30
     echo "$(date): Checking if migrations are complete."
@@ -19,19 +17,4 @@ if [[ $RUN_DB_MIGRATION_BOOLEAN=='1' ]]; then
     echo "$(date): Database migrations are complete"
 fi
 
-
-# creating user for fab auth manager (https://airflow.apache.org/docs/apache-airflow-providers-fab/stable/auth-manager/webserver-authentication.html)
-echo ${CREATE_WEBSERVER_USER_BOOLEAN}
-if [[ $CREATE_WEBSERVER_USER_BOOLEAN==1 ]]; then
-    echo "$(date): Creating admin user for webserver."
-    airflow users create \
-        --username ${AIRFLOW_WEBSERVER_USERNAME} \
-        --password ${AIRFLOW_WEBSERVER_PASSWORD}  \
-        --firstname admin \
-        --lastname admin \
-        --role Admin \
-        --email ${AIRFLOW_WEBSERVER_EMAIL}
-fi
-
-# starting webserver (https://airflow.apache.org/docs/apache-airflow/2.9.0/start.html)
 airflow api-server --port 80
